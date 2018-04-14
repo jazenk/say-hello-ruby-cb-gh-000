4e6868f74cc355ee973d05da84e46209dd22dc30
jake zenk <jake.zenk@ymail.com>
2018-04-14 22:38:17 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,4 @@
 # Build your say_hello method here
-def say_hello (name, language = "Ruby Programmer")
+def say_hello (name)
   puts "Hello #{name}!"
-  puts "We heard you are a great #{language}"
 end
